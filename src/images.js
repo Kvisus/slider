c95df8e662d095d8c0ca306a533819ac5073a1d5
Kvisus <eternal.wanderer.z@gmail.com>
2022-07-20 18:40:46 +0300

    final

---
+++ src/images.js
@@ -0,0 +1,8 @@
+import image1 from './assets/dave-hoefler-HpfM6RFhBbM-unsplash.jpg'
+import image2 from './assets/dave-hoefler-px-tX8dnE-A-unsplash.jpg'
+import image3 from './assets/dave-hoefler-rw2sFOdBszI-unsplash.jpg'
+import image4 from './assets/lorenzo-moschi-XG391m6rH_8-unsplash.jpg'
+import image5 from './assets/madhu-shesharam-EVZxXuOEk3w-unsplash.jpg'
+import image6 from './assets/malte-schmidt-enGr5YbjQKQ-unsplash.jpg'
+
+export default [image1,image2,image3,image4,image5,image6]
